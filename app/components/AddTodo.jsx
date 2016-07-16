76ae207154db4e27691a1a5648ee57a6d646b17f
misterrobot <dham001@ucr.edu>
2016-07-16 02:09:56 +0000

    make AddTodo component plus test

---
+++ app/components/AddTodo.jsx
@@ -0,0 +1,29 @@
+var React = require("react");
+
+var AddTodo = React.createClass({
+    handleSubmit: function(e){
+        e.preventDefault();
+        var todoText = this.refs.todoText.value;
+        
+        if(todoText.length > 0) {
+            this.refs.todoText.value = '';
+            this.props.onAddTodo(todoText);
+        } else {
+            //nothing runs and todoText form gets focused
+            this.refs.todoText.focus();
+        }
+    },
+    
+    render: function(){
+        return(
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" ref="todoText" placeholder="Add item"/>
+                    <button className="button expanded">Add Todo</button>
+                </form>
+            </div>
+        )
+    }
+})
+
+module.exports = AddTodo;
